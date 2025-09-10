0f33e2d20e722cb260b9fd7d76771b466d2b362b
haymanot <haymanotebabu2@gmail.com>
2025-09-10 07:38:25 -0700

    about css commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
-// Components
 import Header from "./component/header/Header";
 import Home from "./component/header/home/Home";
 import Rooms from "./component/header/rooms/Rooms";
 import About from "./component/header/about/About";
-import Footer from "./component/header/footer/Footer";
 import Gallary from "./component/Gallary/Gallary";
 import Contact from "./component/header/contact/Contact";
-function App(){
+import Footer from "./component/header/footer/Footer";
+function App() {
   return (
     <div className="app-container">
-      <Header />
-
+      <Header/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<Rooms />} />
-         <Route path="/about" element={<About />} />
-         <Route path="gallary" element={<Gallary />} />
-         <Route path="contact" element={<Contact/>}/>
-      </Routes>        
-      <Footer />
+          <Route index element={<Home />} />
+          <Route path="rooms" element={<Rooms />} />
+          <Route path="about" element={<About />} />
+          <Route path="gallary" element={<Gallary />} />
+          <Route path="contact" element={<Contact />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
-
 export default App;
